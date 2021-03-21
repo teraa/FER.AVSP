0255367da49e892ea5c60d4314dbadcd7e51ddd5
Tera <24725862+teraa@users.noreply.github.com>
2021-03-21 22:59:02 +0100

    add b task projects

---
+++ LSH.Test/UnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AVSP.Lab1b.Test
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ LSH/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AVSP.Lab1b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
